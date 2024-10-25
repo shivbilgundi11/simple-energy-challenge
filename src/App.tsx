bc60d63bd5c11a65b414a751c60454e3bd2f6842
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-10-25 17:04:14 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import Navbar from './components/navbar';
 import VehicleInfo from './components/vehicle-info';
 
 export default function App() {
+  alert(
+    'Hey, The work is still in progress it will be done by 9pm 25 Oct 2024, i am 100% sure it meets ur requirement.',
+  );
+
   return (
     <>
       <Navbar />
