a47adbca9f864b88ffce473604fc7887d31a2470
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-10-25 11:50:58 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <div className='flex min-h-screen w-full flex-col items-center justify-center gap-y-4'>
+        <h1 className='text-3xl font-bold underline'>Hello world...!</h1>
+
+        <h2 className='text-2xl font-semibold'>SimpleEnergy FE - Challenge</h2>
+      </div>
+    </>
+  );
+}
