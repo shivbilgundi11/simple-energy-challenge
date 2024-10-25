1fbc219e7ef47ebd73de0eb79d581997015ec1e8
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-10-25 12:16:55 +0530

    feat: dark-theme with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from './components/navbar';
+
 export default function App() {
   return (
     <>
+      <Navbar />
       <div className='flex min-h-screen w-full flex-col items-center justify-center gap-y-4'>
         <h1 className='text-3xl font-bold underline'>Hello world...!</h1>
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import { ThemeProvider } from './providers/theme-provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
